e9d04532b7f20c322820403c5b7c55683a83af2c
ilham Suryana <szuryanailham090102@gmail.com>
2024-03-26 22:25:38 +0700

    membuat template front end

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Szuryana</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    {{-- boostrap icon  --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    {{-- my style --}}
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    @include('partials.navbar')
+    {{-- halaman container  --}}
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+    {{-- akhir halaman container --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,3 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-danger">
+   <h1>ini navbar</h1>
+  </nav>
