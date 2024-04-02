6569c861ab45744d130fac1e8b08ee0ff6eefe95
yearina <yearina.mustika@gmail.com>
2024-04-02 22:41:51 +0700

    menambahkan routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/detail', function () {
+    return view('detail');
+});
+
+Route::get('/order', function () {
+    return view('order');
+});
+
+Route::get('/order', function () {
+    return view('login');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+
+
+
