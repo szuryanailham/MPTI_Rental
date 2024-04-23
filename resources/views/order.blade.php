0174019b4be76953cc101058120718b02d267a1d
nrfarah22 <raarznrzt29@gmail.com>
2024-04-23 21:14:01 +0700

    menambahkan alert di order

--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.main')
     @section('container')
+
+    <!-- ALERT START -->
+    <div class="alert alert-success d-flex align-items-center" role="alert">
+      <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+      <div>
+        An example success alert with an icon
+      </div>
+    </div>
+    <!-- ALERT END -->
     <!-- FORM ORDER START -->
 
     <div class="container mt-4 mb-5">
