71a6d4713dc3e7adc245768c377c43cf7dd7347c
yearina <yearina.mustika@gmail.com>
2024-04-30 22:51:49 +0700

    merapikan isi kodingan

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,21 +7,13 @@
             <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent" >
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item me-5">
-<<<<<<< HEAD
-                  <a class="nav-link {{Request::is('home') ? 'active' : ''}}" aria-current="page" href="j">Home</a>
-=======
-                  <a class="nav-link {{Request::is('/') ? 'active' : ''}}" aria-current="page" href="j">Home</a>
->>>>>>> 7d6d9ac582a6dbc418c14802caa08cabbb92cb94
+                  <a class="nav-link {{Request::is('/') ? 'active' : ''}}" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item me-5">
                   <a class="nav-link {{Request::is('profile') ? 'active' : ''}}" href="/profile">Profile</a>
                 </li>
                 <li class="nav-item me-5">
-<<<<<<< HEAD
-                  <a class="nav-link {{Request::is('testimony') ? 'active' : ''}}" href="testimony.html">Testimony</a>
-=======
                   <a class="nav-link {{Request::is('testimony') ? 'active' : ''}}" href="/testimony">Testimony</a>
->>>>>>> 7d6d9ac582a6dbc418c14802caa08cabbb92cb94
                 </li>
                 <li class="nav-item">
                   <img src="assets/Vector.png" alt="">
