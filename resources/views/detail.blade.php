83ef60c4b877637574dd7ccca9517b8516e76141
yearina <yearina.mustika@gmail.com>
2024-04-30 19:08:18 +0700

    read semua data ke admin, memperbaiki form add

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -24,7 +24,7 @@
           </div>
           <div class="row  mt-5 ">
             <div class="col-12 d-flex justify-content-end">
-                <a href="order.html" target="_blank" class="btn btn-dark">Rent Now</a>
+                <a href="/order" target="_blank" class="btn btn-dark">Rent Now</a>
               </div>
           </div>
           
@@ -46,49 +46,4 @@
       <!-- PICTURE DETAIL END -->
     <!-- UNIT DETAIL END -->
 
-    <!-- FOOTER START -->
-    <footer class="foot bg-light text-black text-center pt-4 pb-4">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-6" style="font-size: 30px;">
-            <p>Cahaya Waskitha Transport</p>
-          </div>
-          <div class="col-lg-12 mb-5">
-            <a href="https://maps.app.goo.gl/HsnJf1b2Ks7Stm4V8" target="_blank" style="font-size: 10px; text-decoration: none;">Jl. Nogosari No.2, Nogosari I, Wukirsari, Kec. Imogiri, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55782</a>
-          </div>
-          <div class="row justify-content-center">
-            <div class="col-10 d-flex justify-content-center ">
-              <div class="col-2 ">
-                <a href="https://www.instagram.com/cahaya_waskitha?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">
-                  <img src="assets/Instagram.png" alt="Instagram">
-                </a>
-              </div>
-              <div class="col-2">
-                <a href="" target="_blank">
-                  <img src="assets/Youtube.png" alt="Youtube">
-                </a>
-              </div>
-              <div class="col-2 ">
-                <a href="https://wa.me/628562711129" target="_blank">
-                  <img src="assets/Whatsapp.png" alt="Whatsapp">
-                </a>
-              </div>
-              <div class="col-2">
-                <a href="" target="_blank">
-                  <img src="assets/Facebook.png" alt="Facebook">
-                </a>
-              </div>
-            </div>
-            
-          </div>
-          <div class="col-lg-12 mt-5 text-end">
-            <a href="login_admin.html" style="text-decoration: none; color: black;">Login as Admin</a>
-          </div>
-          <div class="col-lg-12 mt-2">
-            <p>&copy; 2024 CahayaWaskitha. All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <!-- FOOTER END -->
     @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,13 +7,13 @@
             <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent" >
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item me-5">
-                  <a class="nav-link active" aria-current="page" href="j">Home</a>
+                  <a class="nav-link {{Request::is('/') ? 'active' : ''}}" aria-current="page" href="j">Home</a>
                 </li>
                 <li class="nav-item me-5">
-                  <a class="nav-link" href="/profile">Profile</a>
+                  <a class="nav-link {{Request::is('profile') ? 'active' : ''}}" href="/profile">Profile</a>
                 </li>
                 <li class="nav-item me-5">
-                  <a class="nav-link" href="testimony.html">Testimony</a>
+                  <a class="nav-link {{Request::is('testimony') ? 'active' : ''}}" href="/testimony">Testimony</a>
                 </li>
                 <li class="nav-item">
                   <img src="assets/Vector.png" alt="">
