72a2147dcf9d496115c579546193a854e76df715
yearina <yearina.mustika@gmail.com>
2024-06-04 19:40:02 +0700

    menambahkan slug

--- app/Models/Testimony.php
+++ app/Models/Testimony.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Testimony extends Model
 {
     use HasFactory;
+    
+    protected $fillable = ['rating', 'name', 'description'];
 }
--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Unit extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     protected $guard = ['id'];
+    protected $fillable =['name', 'price', 'category_id', 'capacity', 'description', 'slug'];
 
     
     public function category(){
@@ -20,5 +22,20 @@ class Unit extends Model
         return $this->hasMany(Transaction::class);
     }
 
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
     
 }
